949803b63a65a0929da213ad90ffee5f6b195f0b
Jose E. Pimentel <jedpimentel@gmail.com>
2018-10-29 22:19:42 -0400

    es6 classes test

--- main.js
+++ main.js
@@ -14,74 +14,106 @@
 // #	press F to do something
 
 
-const square_color = 'white'
-const text_background = 'gray'
-const std_size = 100
+const square_color = 'gray';
+const text_background = 'gray';
+const std_size = 100;
 
-const MainElement = document.getElementById("main")
-do_stuff();
+const MainElement = document.getElementById("main");
+MainElement.style.backgroundColor = '#AAAAAA ';
 
 // =====
 
-function Square(size_x=std_size, size_y=std_size, pos_x=10, pos_y=10) {
+class Square {
+	constructor(config={}) {
 
-	// is there something better than divs?
-	self.element = document.createElement("div")
-	// self.element.style.width = `${size_x}`
-	self.element.style.width = `${size_x}px`
-	self.element.style.height = `${size_y}px`
-	self.element.style.backgroundColor = square_color
-	self.element.style.display = 'inline-block'
+		// I should really clean this up, using the OR syntax might not really be proper
+		let size_x = config.size_x || std_size;
+		let size_y = config.size_y || std_size;
+		let pos_x = config.pos_x || 10;
+		let pos_y = config.pos_y || 10;
 
-	MainElement
-	MainElement.appendChild(self.element)
+		// is there something better than divs?
+		this.element = document.createElement("div");
 
-	console.log('stuff')
-	return self
+		// Size and fill
+		this.element.style.width = `${size_x}px`;
+		this.element.style.height = `${size_y}px`;
+		this.element.style.opacity = '0.2';
+		this.element.style.backgroundColor = square_color;
+
+		this.element.style.position = 'fixed';
+		this.element.style.display = 'inline-block';
+		this.element.style.left = `${pos_x}px`;
+		this.element.style.top = `${pos_y}px`;
+
+		MainElement.appendChild(this.element);
+
+		console.log('stuff');
+		// return self
+
+	}
 };
 
-function Key(char, x, y) {
+class Circle extends Square {
+	// is the equals thing necessary here?
+	constructor(config={}) {
+		super(config);
+		this.element.style.borderRadius = "50%";
+	}
 
-	self.element = Square(undefined, undefined, x, y).element
-	const text = document.createElement("h1")
-	text.style.fontSize = `${std_size}px`;	
-
-	// maybe make the whole thing have the text background instead of just the h2?
-	text.style.backgroundColor = text_background	
-	text.style.margin = "0"	
-
-	// holy shit, you're peeking into my thought process
-	text.innerHTML = char
-
-	self.element.appendChild(text)
 }
 
-// function char_board(arr_of_arr_of_char_buttons) {
-// 	for(row of arr_of_arr_of_char_buttons) {
-// 		let y_pos = 0;
-// 		for(object of iter_item) {
-// 			space_between_buttons = 123
-// 		}
-// 	}
+
+// not good anymore since Square was made into a class
+// function Key(char, x, y) {
+// 	self.element = Square(undefined, undefined, x, y).element
+// 	const text = document.createElement("h1")
+// 	text.style.fontSize = `${std_size}px`;	
+// 	// maybe make the whole thing have the text background instead of just the h2?
+// 	text.style.backgroundColor = text_background	
+// 	text.style.margin = "0"	
+// 	// holy shit, you're peeking into my thought process
+// 	text.innerHTML = char
+// 	self.element.appendChild(text)
 // }
 
+// let a = new Square()
+new Square();
+
+
 function do_stuff() {
-
 	// Square()
-
 	let loc = 10;
 	const step = 110;
 
-	Key('T', loc); loc += step;
-	Key('H', loc); loc += step;
-	Key('I', loc); loc += step;
-	Key('S', loc); loc += step;
-	Key(' ', loc); loc += step;
-	Key('A', loc); loc += step;
-	Key(' ', loc); loc += step;
-	Key('T', loc); loc += step;
-	Key('E', loc); loc += step;
-	Key('S', loc); loc += step;
-	Key('T', loc); loc += step;
-
+	for(let xi = 0; xi < 3; xi++) {
+		for(let yi = 0; yi < 3; yi++) {
+			let a = new Circle({
+				size_x: 100,
+				size_y: 100,
+				pos_x: 100 + step*xi,
+				pos_y: 100 + step*yi,
+			});
+				// 100
+				// 100
+				// 100 + step*xi
+				// 100 + step*yi);
+		}
+	}
 }
+
+// function main_resize_script() {
+// 	let x_max = document.documentElement.clientWidth;
+// 	let y_max = document.documentElement.clientHeight;
+// 	console.log(x_max, y_max)
+// }
+
+// main_resize_script();
+
+// WARNING !!! This WILL probably need a rate limiter, add the refactor to the "Up Next"
+// window.addEventListener('resize', main_resize_script);
+
+// MAIN "DO STUFF" chunk, apparently it didn't like running before the class definition, 
+// I guess classes arent hoisted like functions are, all this scope stuff is pretty confusing
+
+do_stuff();
