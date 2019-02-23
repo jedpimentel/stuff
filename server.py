c306a5f1b8fcc8e03285220ac1a9e1c0166f5413
Jose E. Pimentel <jedpimentel@gmail.com>
2019-02-23 13:55:55 -0500

    webcam experiment starter code
    works on my desktop, phone didn't ask to use camera :/

--- server.py
+++ server.py
@@ -5,4 +5,9 @@
 # windows: just do:
 # 	py -m http.server 8000
 
-# TODO: actually write this component
+# TODO:
+# - actually write this component
+# - Real backend will be needed to stream the server's webcam
+
+# Nice to haves:
+# something to calculate the repo size, might need a database soon if I keep using scanned drawing
