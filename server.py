195abc99ce83cfa1c7ca8565b84120fd62da5f7c
Jose Eduardo Pimentel <jedpimentel@gmail.com>
2019-04-09 19:34:31 -0400

    image marker test thing

--- server.py
+++ server.py
@@ -5,6 +5,8 @@
 # windows: just do:
 # 	py -m http.server 8000
 
+# https needed for webcam stuff
+
 # TODO:
 # - actually write this component
 # - Real backend will be needed to stream the server's webcam
